94f6db300e9a154b169ec90b71441685b2fabcea
Yurii Koidan <antithesiswork@gmail.com>
2025-03-01 18:19:41 +0200

    Chat room added

--- aschatapp/fastapiapp/routers/chat.py
+++ aschatapp/fastapiapp/routers/chat.py
@@ -5,9 +5,9 @@ from ..utils.websocket import manage_websocket
 router = APIRouter()
 
 
-@router.websocket("/ws/chat")
-async def websocket_endpoint(websocket: WebSocket):
+@router.websocket("/ws/chat/{chat_id}")
+async def websocket_endpoint(websocket: WebSocket, chat_id: str):
     try:
-        await manage_websocket(websocket)
+        await manage_websocket(websocket, chat_id)
     except WebSocketDisconnect:
-        print("Client disconnected")
+        print(f"Client disconnected from chat {chat_id}")
--- aschatapp/fastapiapp/utils/websocket.py
+++ aschatapp/fastapiapp/utils/websocket.py
@@ -1,21 +1,28 @@
-from fastapi import WebSocket, WebSocketDisconnect
 import logging
+from fastapi import WebSocket, WebSocketDisconnect
 
-active_connections = []
+
+active_connections = {}
 
 
-async def manage_websocket(websocket: WebSocket):
+async def manage_websocket(websocket: WebSocket, chat_id: str):
     await websocket.accept()
-    active_connections.append(websocket)
+
+    if chat_id not in active_connections:
+        active_connections[chat_id] = []
+
+    active_connections[chat_id].append(websocket)
 
     try:
         while True:
             message = await websocket.receive_text()
 
-            for connection in active_connections:
+            for connection in active_connections[chat_id]:
                 if connection != websocket:
-                    await connection.send_text(f"Message: {message}")
+                    await connection.send_text(f"Message from chat {chat_id}: {message}")
     except WebSocketDisconnect:
-        active_connections.remove(websocket)
+        active_connections[chat_id].remove(websocket)
         await websocket.close()
-        logging.info("WebSocket connection closed.")
+        logging.info(f"WebSocket connection for chat {chat_id} closed.")
+        if not active_connections[chat_id]:
+            del active_connections[chat_id]
